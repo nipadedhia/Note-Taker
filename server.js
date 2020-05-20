1577c6daf72375a634f77ce2d74a33b9228904f3
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-05-20 14:18:58 -0500

    added routes

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});

aad26a21f5720320012bf6cf6f3d3a7ea154702f
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-05-26 20:22:22 -0500

    get request

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Dependencies
-
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -8,16 +7,28 @@ const app = express();
 
 // Sets an initial port. this later will be use in listener
 var PORT = process.env.PORT || 8080;
+app.listen(PORT);
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routes
+// Html Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+// Set static folder to get css and js files
+app.use(express.static(path.join(__dirname, "public")));
+
+//  GET Request
+app.get("/api/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    var notes = JSON.parse(data);
+    res.send(notes);
+  });
 });

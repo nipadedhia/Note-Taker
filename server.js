61c646e57cbf1c17b733a88fcaef0be004fb19df
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-05-26 21:40:15 -0500

    moved api and html routes to separate file

--- server.js
+++ server.js
@@ -3,60 +3,20 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+// Create an express server.
 const app = express();
 
 // Sets an initial port. this later will be use in listener
 var PORT = process.env.PORT || 8080;
+
+// Listen on port.
 app.listen(PORT);
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Html Routes
-// If no matching route is found default to home page.
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-// Link to css file.
-app.get("/styles", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/css/styles.css"));
-});
-
-//  GET Request
-app.get("/api/notes", function (req, res) {
-  return res.json(data);
-});
-
-// POST Request
-app.post("/api/notes", function (req, res) {
-  data.push(req.body);
-  writeData();
-  return res.json(data);
-});
-
-// DELETE Request
-app.delete("/api/notes/:id", function (req, res) {
-  const id = req.params.id;
-  data = data.filter(function (note) {
-    if (note.id === id) {
-      return false;
-    }
-    return true;
-  });
-  writeData();
-  return res.json(data);
-});
-
-// Write data
-const writeData = function () {
-  fs.writeFile("./db/db.json", JSON.stringify(data), (err) => {
-    if (err) throw err;
-  });
-};
-let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+// ROUTES
+// Point the server to the route files.
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);

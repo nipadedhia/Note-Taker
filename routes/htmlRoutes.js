2d5de0251f0f875017906646094080a181923cb6
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-05-28 14:17:52 -0500

    replaced * with /

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,7 +13,7 @@ module.exports = function (app) {
   });
 
   // If no matching route is found default to home page.
-  app.get("*", function (req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };

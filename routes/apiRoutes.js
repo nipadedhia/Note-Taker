6e1834c0acc555b43cbed51340146d68d44404b3
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-05-28 13:12:38 -0500

    added comment

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,39 @@
-// If no matching route is found default to home page.
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// Required Modules
+const fs = require("fs");
+const notesData = require("../db/db.json");
 
-//  GET Request
-app.get("/api/notes", function (req, res) {
-  return res.json(data);
-});
-
-// POST Request
-app.post("/api/notes", function (req, res) {
-  data.push(req.body);
-  writeData();
-  return res.json(data);
-});
-
-// DELETE Request
-app.delete("/api/notes/:id", function (req, res) {
-  const id = req.params.id;
-  data = data.filter(function (note) {
-    if (note.id === id) {
-      return false;
-    }
-    return true;
+// Routing
+module.exports = function (app) {
+  //  GET Request
+  app.get("/api/notes", function (req, res) {
+    return res.json(data);
   });
-  writeData();
-  return res.json(data);
-});
 
-// Write data
-const writeData = function () {
-  fs.writeFile("./db/db.json", JSON.stringify(data), (err) => {
-    if (err) throw err;
+  // POST Request
+  app.post("/api/notes", function (req, res) {
+    data.push(req.body);
+    writeData();
+    return res.json(data);
   });
+
+  // DELETE Request
+  app.delete("/api/notes/:id", function (req, res) {
+    const id = req.params.id;
+    data = data.filter(function (note) {
+      if (note.id === id) {
+        return false;
+      }
+      return true;
+    });
+    writeData();
+    return res.json(data);
+  });
+
+  // Write data
+  const writeData = function () {
+    fs.writeFile("./db/db.json", JSON.stringify(data), (err) => {
+      if (err) throw err;
+    });
+  };
+  let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 };
-let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));

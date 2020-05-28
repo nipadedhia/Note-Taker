634911cb58d33346f8a6bd0aa19a59c894c4825e
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-05-28 12:48:08 -0500

    console log for PORT listening

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const app = express();
 // Sets an initial port. this later will be use in listener
 var PORT = process.env.PORT || 8080;
 
-// Listen on port.
-app.listen(PORT);
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,3 +17,9 @@ app.use(express.json());
 // Point the server to the route files.
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+
+// Listen on port.
+// app.listen(PORT);
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});

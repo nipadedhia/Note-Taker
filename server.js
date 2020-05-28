7c4568ab3c269093147f8b8de30c3199a7d0deda
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-05-28 15:12:23 -0500

    replaced var with const for port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const app = express();
 
 // Sets an initial port. this later will be use in listener
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
